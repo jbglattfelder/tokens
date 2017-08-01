c161f7fb83154f394a65e32d258a23e2117b0c59
James Glattfelder <jbglattfelder@users.noreply.github.com>
2017-08-01 15:51:53 +0200

    Create example.sol

---
+++ example.sol
@@ -0,0 +1,35 @@
+// See http://j-node.blogspot.ch/2017/08/how-to-ico.html
+pragma solidity ^0.4.10;
+
+contract Example {
+    address owner;
+    struct Account {
+        string addr;
+        uint amount;
+    }
+    mapping (uint => Account) accounts;
+    
+    // Constructor
+    function Example() {
+        owner = msg.sender;
+        require(owner == msg.sender);
+        Message("OK");
+    }
+
+    // Setter and getter
+    function set(uint _index, string _addr, uint _amount) {
+        accounts[_index].addr = _addr;
+        accounts[_index].amount = _amount;
+    }
+    
+    function getAmount(uint _index) constant returns (uint) {
+        return accounts[_index].amount;
+    }
+    
+    function getAmddr(uint _index) constant returns (string) {
+        return accounts[_index].addr;
+    }
+    
+    // Message
+    event Message(string mymsg);
+}
